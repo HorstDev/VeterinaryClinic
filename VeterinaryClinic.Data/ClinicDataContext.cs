44fd0717454c7f101bbe1a5664e949141ddde1e7
HorstDev <gorstsergei@mail.ru>
2022-12-05 22:36:16 +0300

    Add database (db name = VetClinic)

---
+++ VeterinaryClinic.Data/ClinicDataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VeterinaryClinic.Data
+{
+    public class ClinicDataContext : DbContext
+    {
+        public ClinicDataContext(DbContextOptions<ClinicDataContext> options)
+            : base(options)
+        {
+
+        }
+
+        // Сразу в базу данных добавляем роли
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            const string petOwnerRoleName = "PetOwner";
+            const string adminRoleName = "Admin";
+
+            Role PetOwnerRole = new Role { Id = 1, Name = petOwnerRoleName };
+            Role AdminRole = new Role { Id = 2, Name = adminRoleName };
+
+            modelBuilder.Entity<Role>().HasData(new Role[] { PetOwnerRole, AdminRole });
+
+            modelBuilder.UseSerialColumns();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
--- VeterinaryClinic/Program.cs
+++ VeterinaryClinic/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using VeterinaryClinic.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ClinicDataContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("VetClinicDb"))
+    );
 
 var app = builder.Build();
 
